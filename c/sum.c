923840b712e8bae0d6393b4de13741533a4bba7b
Vadim Radionov <vadim.radionov@gmail.com>
2019-03-16 21:45:43 +0300

    function sum

---
+++ c/sum.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int sum(int a, int b) { return a+b; }
+
+int main() {
+  int result = sum(5, 7);
+  printf("%d\n", result);
+}
---
+++ js/sum.js
@@ -0,0 +1,4 @@
+function sum(a, b) { return a+b; }
+let result = sum(5, 7)
+console.log(result)
+
