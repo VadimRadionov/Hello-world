d11934a537429ce2c3dd2e1d5ef243b26c0f6050
Vadim Radionov <vadim.radionov@gmail.com>
2019-03-20 00:25:06 +0300

    Add gender query, initialize low and high bounds (with same defaults)

--- c/bmi.c
+++ c/bmi.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
 float calc_bmi(float h, float w) {
   return w/(h*h);
 }
 
 void main() {
-  float height, weight, bmi;
+  float height, weight, bmi, bmi_low, bmi_high ;
+  char gender[4] ;
+
+  printf("Укажите пол (м/ж или m/f): ");
+  scanf("%s", gender);
+
+  if (!strcmp(gender, "м") || !strcmp(gender, "m"))
+    { bmi_low = 18.5; bmi_high = 25; }
+  else
+    { bmi_low = 18.5; bmi_high = 25; }
+
   
   printf("Введите рост в метрах: ");
   scanf("%f", &height);
@@ -14,12 +25,12 @@ void main() {
 
   bmi = calc_bmi(height, weight);
 
-  if (bmi < 18.5)
-    { printf("BMI = %.1f ниже нижней границы нормы\n", bmi); }
-  else if (bmi > 25)
-    { printf("BMI = %.1f выше верхней границы нормы\n", bmi); }
+  if (bmi < bmi_low)
+    { printf("BMI = %.1f ниже нижней границы нормы (%.1f)\n", bmi, bmi_low); }
+  else if (bmi > bmi_high)
+    { printf("BMI = %.1f выше верхней границы нормы (%.1f)\n", bmi, bmi_high); }
   else
-    { printf("BMI = %.1f в пределах нормы\n", bmi); }
+    { printf("BMI = %.1f в пределах нормы (%.1f–%.1f)\n", bmi, bmi_low, bmi_high); }
   
 }
   
