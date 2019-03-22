791cfa6be56b93064c673c3e84befc3fc2824322
Vadim Radionov <vadim.radionov@gmail.com>
2019-03-23 01:10:57 +0300

    Cleanup

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,8 +1,5 @@
-a = "abba"
+a = "ab"
 
-function is_palindrome(a) {
-    if (a.length < 2) { return true; } 
-    return (a[0] == a[a.length-1] && is_palindrome (a.substr(1,a.length-2)))
-}
+function is_palindrome(a) { return (a.length < 2 || ( a[0] == a[a.length-1] && is_palindrome (a.substr(1,a.length-2)))) }
     
 console.log(is_palindrome(a))
