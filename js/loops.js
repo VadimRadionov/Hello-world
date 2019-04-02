68c5bb2d1dfa0440f5b6931a720e1d947e203c12
Vadim Radionov <vadim.radionov@gmail.com>
2019-04-02 21:22:05 +0300

    Set correct initial value

--- js/loops.js
+++ js/loops.js
@@ -2,11 +2,11 @@ var a = [1,1,2,3,5,8,13,21];
 
 // Loops
 
-//var i = 0, sum = 0;
-//while (i < a.length) { sum += a[i]; i++; }
-//for (i = 0, prod = 1; i < a.length; i +=2) { prod *= a[i]; }
+var i = 0, sum = 0;
+while (i < a.length) { sum += a[i]; i++; }
+for (i = 0, prod = 1; i < a.length; i +=2) { prod *= a[i]; }
 
-//console.log(sum, prod);
+console.log(sum, prod);
 
 // Array methods
 
